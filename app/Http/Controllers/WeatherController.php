e4e61e4e54c24ab602e1bc3c4d3baf7d811c7bba
Roman Komoliev <komolev@gmail.com>
2022-08-31 21:51:19 +0100

    Add weather provider to weather controller

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\WeatherProviderInterface;
 use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    public function __construct()
+    /** @var \App\Contracts\WeatherProviderInterface */
+    protected $weatherProvider;
+
+    public function __construct(WeatherProviderInterface $weatherProvider)
     {
+        $this->weatherProvider = $weatherProvider;
+
         $this->middleware('auth:api');
     }
 
     public function showCityWeather(Request $request)
     {
+        $requestData = $request->all();
+
+        $weatherText = $this->weatherProvider->getLocationWeather($requestData['city']);
+
         return response()->json(
             [
-                "weather"  => 'Good',
+                "weather"  => $weatherText,
             ],
             200
         );
